6ca38ce741050a6d989954131aa9f656ea852364
Kalem S <kalemschrock@gmail.com>
2023-07-21 12:23:18 -0500

    adding more pages for select page

--- Kalems_dev_idk/main.js
+++ Kalems_dev_idk/main.js
@@ -6,6 +6,9 @@ const book = document.querySelector("#book");
 const paper1 = document.querySelector("#p1");
 const paper2 = document.querySelector("#p2");
 const paper3 = document.querySelector("#p3");
+const paper4 = document.querySelector("#p4");
+const paper5 = document.querySelector("#p5");
+const paper6 = document.querySelector("#p6");
 
 // Event Listener
 prevBtn.addEventListener("click", goPrevPage);
@@ -13,7 +16,7 @@ nextBtn.addEventListener("click", goNextPage);
 
 // Business Logic
 let currentLocation = 1;
-let numOfPapers = 3;
+let numOfPapers = 6;
 let maxLocation = numOfPapers + 1;
 
 function openBook() {
@@ -34,20 +37,42 @@ function closeBook(isAtBeginning) {
 }
 
 function goNextPage() {
+    //console.log(currentLocation)
     if(currentLocation < maxLocation) {
         switch(currentLocation) {
             case 1:
                 openBook();
                 paper1.classList.add("flipped");
-                paper1.style.zIndex = 1;
+                setTimeout(function() {
+                    paper1.style.zIndex = 1;
+                    console.log("This line will execute after 0.5 seconds.");
+                  }, 250); // 500 milliseconds = 0.5 seconds
                 break;
             case 2:
                 paper2.classList.add("flipped");
-                paper2.style.zIndex = 2;
+                setTimeout(function() {
+                    paper2.style.zIndex = 2;
+                    console.log("This line will execute after 0.5 seconds.");
+                  }, 250); // 500 milliseconds = 0.5 seconds
                 break;
             case 3:
                 paper3.classList.add("flipped");
-                paper3.style.zIndex = 3;
+                setTimeout(function() {
+                    paper3.style.zIndex = 3;
+                    console.log("This line will execute after 0.5 seconds.");
+                  }, 250); // 500 milliseconds = 0.5 seconds
+                break;
+            case 4:
+                paper4.classList.add("flipped");
+                paper4.style.zIndex = 4;
+                break;
+            case 5:
+                paper5.classList.add("flipped");
+                paper5.style.zIndex = 5;
+                break;
+            case 6:
+                paper6.classList.add("flipped");
+                paper6.style.zIndex = 6;
                 closeBook(false);
                 break;
             default:
@@ -58,21 +83,43 @@ function goNextPage() {
 }
 
 function goPrevPage() {
+    //console.log(currentLocation)
     if(currentLocation > 1) {
         switch(currentLocation) {
             case 2:
                 closeBook(true);
                 paper1.classList.remove("flipped");
-                paper1.style.zIndex = 3;
+                paper1.style.zIndex = 6;
                 break;
             case 3:
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex = 2;
+                paper2.style.zIndex = 5;
                 break;
             case 4:
-                openBook();
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
+                paper3.style.zIndex = 4;
+                break;
+            case 5:
+                paper4.classList.remove("flipped");
+                setTimeout(function() {
+                    paper4.style.zIndex = 3;
+                    console.log("This line will execute after 0.5 seconds.");
+                  }, 500); // 500 milliseconds = 0.5 seconds
+                break;
+            case 6:
+                setTimeout(function() {
+                    paper5.style.zIndex = 2;
+                    console.log("This line will execute after 0.5 seconds.");
+                  }, 250); // 500 milliseconds = 0.5 seconds
+                paper5.classList.remove("flipped");
+                break;
+            case 7:
+                openBook();
+                setTimeout(function() {
+                    paper6.style.zIndex = 1;
+                    console.log("This line will execute after 0.5 seconds.");
+                  }, 250); // 500 milliseconds = 0.5 seconds
+                paper6.classList.remove("flipped");
                 break;
             default:
                 throw new Error("unkown state");
